29731161ba69f601ea7442d0b1d0e28b9da7ef0f
kabbo25 <kabbo25@gmail.com>
2024-03-31 10:29:14 +0600

    add loading spinner

--- src/pages/UserProfilePage.tsx
+++ src/pages/UserProfilePage.tsx
@@ -1,16 +1,16 @@
 import UserProfileForm from "@/forms/user_profile_form/UserProfileForm.tsx";
 import {UseGetMyUser, UseUpdateUser} from "@/api/MyUserApi.tsx";
-import { RotatingLines} from "react-loader-spinner";
+import {RotatingLines} from "react-loader-spinner";
 
 const UserProfilePage = () => {
     const {currentUser, isLoading: isGetLoading} = UseGetMyUser();
     const {updateUser, isLoading: isUpdateLoading} = UseUpdateUser();
     if (isGetLoading) {
         return <RotatingLines
-            height="80"
+
             width="80"
-            radius="9"
-            color="green"
+
+
             ariaLabel="loading"
         />
 
