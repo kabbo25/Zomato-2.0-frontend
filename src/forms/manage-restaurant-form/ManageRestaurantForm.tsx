a6c94db20e7ae34c2da08ae77c42c11dd5175209
kabbo25 <kabbo25@gmail.com>
2024-04-23 09:47:20 +0600

    feat: Send form data as Multipart form data and save it to form .

--- src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
+++ src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -5,15 +5,20 @@ import {Form} from "@/components/ui/form.tsx";
 import DetailsSection from "@/forms/manage-restaurant-form/DetailsSection.tsx";
 import CuisinesSection from "@/forms/manage-restaurant-form/CuisinesSection.tsx";
 import {Separator} from "@/components/ui/separator.tsx";
-const formSchema=z.object({
-   RestaurantName:  z.string({
-       required_error: "Restaurant name is required"
-   }),
+import MenuSection from "@/forms/manage-restaurant-form/MenuSection.tsx";
+import ImageSection from "@/forms/manage-restaurant-form/ImageSection.tsx";
+import LoadingButton from "@/components/LoadButton.tsx";
+import {Button} from "@/components/ui/button.tsx";
+
+const formSchema = z.object({
+    RestaurantName: z.string({
+        required_error: "Restaurant name is required"
+    }),
     City: z.string({
         required_error: "City is required"
     }),
     Country: z.string({
-        required_error:"Country is required"
+        required_error: "Country is required"
     }),
     DeliveryTime: z.coerce.number({
         required_error: "Estimated delivery time is required",
@@ -27,18 +32,23 @@ const formSchema=z.object({
         message: "Cuisine should be array"
     }),
     MenuItems: z.array(z.object({
-        Name: z.string().min(1,"Menu Name is required"),
-        Price: z.coerce.number().min(1,"Menu price should be valid integer")
+        Name: z.string().min(1, "Menu Name is required"),
+        Price: z.coerce.number().min(1, "Menu price should be valid integer")
     })),
-    ImageURl: z.instanceof(File,{message: "image file is missing"})
+    ImageUrl: z.string().optional(),
+    ImageFile: z.instanceof(File, {message: "image is required"}).optional(),
 })
-type restaurantFormData=z.infer<typeof formSchema>;
-type Props={
-    onSave: (restaurantFromData: FormData)=>void ;
+    .refine((data) => data.ImageUrl || data.ImageFile, {
+        message: "Either image URL or image File must be provided",
+        path: ["ImageFile"],
+    });
+type RestaurantFormData = z.infer<typeof formSchema>;
+type Props = {
+    onSave: (restaurantFromData: FormData) => void;
     isLoading: boolean;
 };
-const ManageRestaurantForm = ({onSave,isLoading}:Props) => {
-    const form=useForm<restaurantFormData>({
+const ManageRestaurantForm = ({onSave, isLoading}: Props) => {
+    const form = useForm<RestaurantFormData>({
         resolver: zodResolver(formSchema),
         defaultValues: {
             Cuisine: [],
@@ -48,19 +58,62 @@ const ManageRestaurantForm = ({onSave,isLoading}:Props) => {
         }
     })
 
-    const onSubmit=(formDataJson: restaurantFormData)=>{
-
+    const onSubmit = (formDataJson: RestaurantFormData) => {
+        const formData = new FormData();
+        formData.append("RestaurantName", formDataJson.RestaurantName);
+        formData.append("City", formDataJson.City);
+        formData.append("Country", formDataJson.Country);
+        formData.append(
+            "DeliveryPrice",
+            (formDataJson.DeliveryPrice * 100).toString()
+        );
+        formData.append(
+            "DeliveryTime",
+            formDataJson.DeliveryTime.toString()
+        );
+        formDataJson.Cuisine.forEach((item, index)=>{
+            formData.append(`Cuisine[${index}]`, item);
+        });
+        formDataJson.MenuItems.forEach((item,index)=>{
+            formData.append(`MenuItems[${index}][Name]`,item.Name);
+            formData.append(
+                `MenuItems[${index}][Price]`,
+                (item.Price*100).toString()
+            );
+        });
+        if (formDataJson.ImageFile) {
+            formData.append(`ImageFile`, formDataJson.ImageFile);
+        }
+        onSave(formData);
     }
 
     return (
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)}
-                className={'bg-gray-50 space-y-4 p-10 rounded-lg'}>
-                <DetailsSection/>
-                <Separator/>
-                <CuisinesSection/>
-            </form>
-        </Form>
+        <>
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)}
+                      className={'bg-gray-50 space-y-4 p-10 rounded-lg'}>
+                    <DetailsSection/>
+                    <Separator/>
+                    <CuisinesSection/>
+                    <Separator/>
+                    <MenuSection/>
+                    <Separator/>
+                    <ImageSection/>
+                    {
+                        isLoading ?
+                            (
+                                <LoadingButton/>
+                            ) :
+                            (
+                                <Button type={'submit'} className={'w-[10%] font-bold'}>
+                                    Submit
+                                </Button>
+                            )
+                    }
+                </form>
+
+            </Form>
+        </>
 
     )
 }
