82984fc01e1b3b5233710949675ad16075642043
kabbo25 <kabbo25@gmail.com>
2024-04-26 23:09:07 +0600

    feat: implement restaurant create API

--- src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
+++ src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -1,115 +1,114 @@
 import LoadingButton from "@/components/LoadButton.tsx";
-import { Button } from "@/components/ui/button.tsx";
-import { Form } from "@/components/ui/form.tsx";
-import { Separator } from "@/components/ui/separator.tsx";
+import {Button} from "@/components/ui/button.tsx";
+import {Form} from "@/components/ui/form.tsx";
+import {Separator} from "@/components/ui/separator.tsx";
 import CuisinesSection from "@/forms/manage-restaurant-form/CuisinesSection.tsx";
 import DetailsSection from "@/forms/manage-restaurant-form/DetailsSection.tsx";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
+import MenuSection from "@/forms/manage-restaurant-form/MenuSection.tsx";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {useForm} from "react-hook-form";
+import {z} from "zod";
+import ImageSection from "@/forms/manage-restaurant-form/ImageSection.tsx";
 
-const formSchema = z.object({
-	RestaurantName: z.string({
-		required_error: "Restaurant name is required",
-	}),
-	City: z.string({
-		required_error: "City is required",
-	}),
-	Country: z.string({
-		required_error: "Country is required",
-	}),
-	DeliveryTime: z.coerce.number({
-		required_error: "Estimated delivery time is required",
-		invalid_type_error: "typed should be number",
-	}),
-	DeliveryPrice: z.coerce.number({
-		required_error: "Delivery Price should be required",
-		invalid_type_error: "typed should be number",
-	}),
-	Cuisine: z.array(z.string()).nonempty({
-		message: "Cuisine should be array",
-	}),
-	// MenuItems: z.array(z.object({
-	//     Name: z.string().min(1, "Menu Name is required"),
-	//     Price: z.coerce.number().min(1, "Menu price should be valid integer")
-	// })),
-	// ImageUrl: z.string().optional(),
-	// ImageFile: z.instanceof(File, {message: "image is required"}).optional(),
-});
-// .refine((data) => data.ImageUrl || data.ImageFile, {
-//     message: "Either image URL or image File must be provided",
-//     path: ["ImageFile"],
-// });
+const formSchema = z
+    .object({
+        RestaurantName: z.string({
+            required_error: "Restaurant name is required",
+        }),
+        City: z.string({
+            required_error: "City is required",
+        }),
+        Country: z.string({
+            required_error: "Country is required",
+        }),
+        DeliveryTime: z.coerce.number({
+            required_error: "Estimated delivery time is required",
+            invalid_type_error: "typed should be number",
+        }),
+        DeliveryPrice: z.coerce.number({
+            required_error: "Delivery Price should be required",
+            invalid_type_error: "typed should be number",
+        }),
+        Cuisine: z.array(z.string()).nonempty({
+            message: "Cuisine should be array",
+        }),
+        MenuItems: z.array(
+            z.object({
+                Name: z.string().min(1, "name is required"),
+                Price: z.coerce.number().min(1, "price is required"),
+            })
+        ),
+        ImageUrl: z.string().optional(),
+        ImageFile: z.instanceof(File, {message: "image is required"}).optional(),
+    })
+    .refine((data) => data.ImageUrl || data.ImageFile, {
+        message: "Either image URL or image File must be provided",
+        path: ["ImageFile"],
+    });
 type RestaurantFormData = z.infer<typeof formSchema>;
 type Props = {
-	onSave: (restaurantFromData: FormData) => void;
-	isLoading: boolean;
+    onSave: (restaurantFromData: FormData) => void;
+    isLoading: boolean;
 };
-const ManageRestaurantForm = ({ onSave, isLoading }: Props) => {
-	const form = useForm<RestaurantFormData>({
-		resolver: zodResolver(formSchema),
-		defaultValues: {
-			Cuisine: [],
-			// MenuItems: [
-			// 	{
-			// 		Name: "Burger",
-			// 		Price: 0,
-			// 	},
-			// ],
-		},
-	});
+const ManageRestaurantForm = ({onSave, isLoading}: Props) => {
+    const form = useForm<RestaurantFormData>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            Cuisine: [],
+        },
+    });
 
-	const onSubmit = (formDataJson: RestaurantFormData) => {
-		console.log(formDataJson);
-		const formData = new FormData();
-		formData.append("RestaurantName", formDataJson.RestaurantName);
-		formData.append("City", formDataJson.City);
-		formData.append("Country", formDataJson.Country);
-		formData.append(
-			"DeliveryPrice",
-			(formDataJson.DeliveryPrice * 100).toString()
-		);
-		formData.append("DeliveryTime", formDataJson.DeliveryTime.toString());
-		formDataJson.Cuisine.forEach((item, index) => {
-			formData.append(`Cuisine[${index}]`, item);
-		});
-		// formDataJson.MenuItems.forEach((item, index) => {
-		// 	formData.append(`MenuItems[${index}][Name]`, item.Name);
-		// 	formData.append(
-		// 		`MenuItems[${index}][Price]`,
-		// 		(item.Price * 100).toString()
-		// 	);
-		// });
-		// if (formDataJson.ImageFile) {
-		// 	formData.append(`ImageFile`, formDataJson.ImageFile);
-		// }
-		onSave(formData);
-	};
+    const onSubmit = (formDataJson: RestaurantFormData) => {
+        console.log(formDataJson);
+        const formData = new FormData();
+        formData.append("RestaurantName", formDataJson.RestaurantName);
+        formData.append("City", formDataJson.City);
+        formData.append("Country", formDataJson.Country);
+        formData.append(
+            "DeliveryPrice",
+            (formDataJson.DeliveryPrice * 100).toString()
+        );
+        formData.append("DeliveryTime", formDataJson.DeliveryTime.toString());
+        formDataJson.Cuisine.forEach((item, index) => {
+            formData.append(`Cuisine[${index}]`, item);
+        });
+        formDataJson.MenuItems.forEach((item, index) => {
+            formData.append(`MenuItems[${index}][Name]`, item.Name);
+            formData.append(
+                `MenuItems[${index}][Price]`,
+                (item.Price * 100).toString()
+            );
+        });
+        if (formDataJson.ImageFile) {
+            formData.append(`ImageFile`, formDataJson.ImageFile);
+        }
+        onSave(formData);
+    };
 
-	return (
-		<>
-			<Form {...form}>
-				<form
-					onSubmit={form.handleSubmit(onSubmit)}
-					className={"bg-gray-50 space-y-4 p-10 rounded-lg"}
-				>
-					<DetailsSection />
-					<Separator />
-					<CuisinesSection />
-					{/* <Separator />
-					<MenuSection />
-					<Separator />
-					<ImageSection /> */}
-					{isLoading ? (
-						<LoadingButton />
-					) : (
-						<Button type={"submit"} className={"w-[10%] font-bold"}>
-							Submit
-						</Button>
-					)}
-				</form>
-			</Form>
-		</>
-	);
+    return (
+        <>
+            <Form {...form}>
+                <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className={"bg-gray-50 space-y-4 p-10 rounded-lg"}
+                >
+                    <DetailsSection/>
+                    <Separator/>
+                    <CuisinesSection/>
+                    <Separator/>
+                    <MenuSection/>
+                    <Separator/>
+                    <ImageSection/>
+                    {isLoading ? (
+                        <LoadingButton/>
+                    ) : (
+                        <Button type={"submit"} className={"w-[10%] font-bold"}>
+                            Submit
+                        </Button>
+                    )}
+                </form>
+            </Form>
+        </>
+    );
 };
 export default ManageRestaurantForm;
--- src/forms/manage-restaurant-form/MenuItemInput.tsx
+++ src/forms/manage-restaurant-form/MenuItemInput.tsx
@@ -1,46 +1,65 @@
-import {Button} from "@/components/ui/button";
-import {FormControl, FormField, FormItem, FormLabel, FormMessage,} from "@/components/ui/form";
-import {Input} from "@/components/ui/input";
-import {useFormContext} from "react-hook-form";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faBangladeshiTakaSign} from "@fortawesome/free-solid-svg-icons";
+import { Button } from "@/components/ui/button";
+import {
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useFormContext } from "react-hook-form";
 
 type Props = {
-    index: number;
-    removeMenuItem: () => void;
+	index: number;
+	removeMenuItem: () => void;
 };
 
-const MenuItemInput = ({index, removeMenuItem}: Props) => {
-    const {control} = useFormContext();
+const MenuItemInput = ({ index, removeMenuItem }: Props) => {
+	const { control } = useFormContext();
 
-    return (
-        <div className="flex flex-row items-end gap-2">
-            <FormField control={control} name={`MenuItems.Name.${index}`} render={({field}) => (
-                <FormItem>
-                    <FormLabel>Name</FormLabel>
-                    <FormMessage/>
-                    <FormControl>
-                        <Input {...field} type={'text'} placeholder={'cheese pizza'}/>
-                    </FormControl>
-                </FormItem>
-            )}/>
-            <FormField control={control} name={`MenuItems.Price.${index}`} render={({field}) => (
-                <FormItem>
-                    <FormLabel>Price (<FontAwesomeIcon icon={faBangladeshiTakaSign} />)</FormLabel>
-                    <FormMessage/>
-                    <FormControl>
-                        <Input {...field} type={'text'} placeholder={'8.00'}/>
-                    </FormControl>
-                </FormItem>
-            )}/>
-            <Button className={'bg-red-500 '} onClick={() => {
-                removeMenuItem();
-            }}>
-                Remove
-            </Button>
-        </div>
-
-    );
+	return (
+		<div className="flex flex-row items-end gap-2">
+			<FormField
+				control={control}
+				name={`MenuItems.${index}.Name`}
+				render={({ field }) => (
+					<FormItem>
+						<FormLabel className="flex items-center gap-1">
+							Name <FormMessage />
+						</FormLabel>
+						<FormControl>
+							<Input
+								{...field}
+								placeholder="Cheese Pizza"
+								className="bg-white"
+							/>
+						</FormControl>
+					</FormItem>
+				)}
+			/>
+			<FormField
+				control={control}
+				name={`MenuItems.${index}.Price`}
+				render={({ field }) => (
+					<FormItem>
+						<FormLabel className="flex items-center gap-1">
+							Price (£) <FormMessage />
+						</FormLabel>
+						<FormControl>
+							<Input {...field} placeholder="8.00" className="bg-white" />
+						</FormControl>
+					</FormItem>
+				)}
+			/>
+			<Button
+				type="button"
+				onClick={removeMenuItem}
+				className="bg-red-500 max-h-fit"
+			>
+				Remove
+			</Button>
+		</div>
+	);
 };
 
 export default MenuItemInput;
