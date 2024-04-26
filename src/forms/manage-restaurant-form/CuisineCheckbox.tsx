5d88a565956213c1a7bdfefdc0d0edc46b8f7708
kabbo25 <kabbo25@gmail.com>
2024-04-26 17:26:38 +0600

    cuise  and detailed section working

--- src/forms/manage-restaurant-form/CuisineCheckbox.tsx
+++ src/forms/manage-restaurant-form/CuisineCheckbox.tsx
@@ -1,30 +1,33 @@
-import {ControllerRenderProps, FieldValues} from "react-hook-form";
-import {FormControl, FormItem, FormLabel} from "@/components/ui/form.tsx";
-import {Checkbox} from "@/components/ui/checkbox.tsx";
+import { Checkbox } from "@/components/ui/checkbox.tsx";
+import { FormControl, FormItem, FormLabel } from "@/components/ui/form.tsx";
+import type { ControllerRenderProps, FieldValues } from "react-hook-form";
 
 type Props = {
-    cuisine: string,
-    field: ControllerRenderProps<FieldValues, "Cuisine">
-}
-const CuisineCheckbox = ({cuisine, field}: Props) => {
-    return (
-        <FormItem className={' flex flex-row items-center space-x-1 space-y-0 mt-2'}>
-            <FormControl>
-                <Checkbox
-                          className="bg-white"
-                          checked={field.value.includes(cuisine)}
-                          onCheckedChange={(checked) => {
-                              if (checked) {
-                                  field.onChange([...field.value, cuisine]);
-                              } else {
-                                  field.onChange(
-                                      field.value.filter((value: string) => value !== cuisine)
-                                  );
-                              }
-                          }}/>
-                    </FormControl>
-                    <FormLabel className="text-sm font-normal">{cuisine}</FormLabel>
-        </FormItem>
-    )
-}
-export default CuisineCheckbox
+	cuisine: string;
+	field: ControllerRenderProps<FieldValues, "Cuisine">;
+};
+const CuisineCheckbox = ({ cuisine, field }: Props) => {
+	return (
+		<FormItem
+			className={" flex flex-row items-center space-x-1 space-y-0 mt-2"}
+		>
+			<FormControl>
+				<Checkbox
+					className="bg-white"
+					checked={field.value.includes(cuisine)}
+					onCheckedChange={(checked) => {
+						if (checked) {
+							field.onChange([...field.value, cuisine]);
+						} else {
+							field.onChange(
+								field.value.filter((value: string) => value !== cuisine)
+							);
+						}
+					}}
+				/>
+			</FormControl>
+			<FormLabel className="text-sm font-normal">{cuisine}</FormLabel>
+		</FormItem>
+	);
+};
+export default CuisineCheckbox;
--- src/forms/manage-restaurant-form/DetailsSection.tsx
+++ src/forms/manage-restaurant-form/DetailsSection.tsx
@@ -1,71 +1,97 @@
-import {FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form.tsx";
+import {
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "@/components/ui/form.tsx";
 
-import {Input} from "@/components/ui/input.tsx";
-import {useFormContext} from "react-hook-form";
-import {faBangladeshiTakaSign} from "@fortawesome/free-solid-svg-icons";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {DevTool} from "@hookform/devtools";
+import { Input } from "@/components/ui/input.tsx";
+import { faBangladeshiTakaSign } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { DevTool } from "@hookform/devtools";
+import { useFormContext } from "react-hook-form";
 
 const DetailsSection = () => {
-    const {control} = useFormContext();
-    return (
-        <div className={'space-y-2'}>
-            <div>
-                <h1 className={'text-2xl font-bold'}>Details</h1>
-            </div>
-            <FormDescription>
-                Enter the details about your restaurant
-            </FormDescription>
-            <FormField control={control} name={"RestaurantName"} render={({field}) => (
-                <FormItem>
-                    <FormLabel>Name</FormLabel>
-                    <FormControl>
-                        <Input placeholder={'kabbo ghosh'} {...field}/>
-                    </FormControl>
-                    <FormMessage/>
-                </FormItem>
-            )}/>
-            <div className={'flex gap-4'}>
-            <FormField control={control} name={"City"} render={({field}) => (
-                <FormItem className={'flex-1'}>
-                    <FormLabel>City</FormLabel>
-                    <FormControl>
-                        <Input placeholder={'Jashore'} {...field}/>
-                    </FormControl>
-                    <FormMessage/>
-                </FormItem>
-
-            )}/>
-            <FormField control={control} name={"Country"} render={({field}) => (
-                <FormItem className={'flex-1'}>
-                    <FormLabel>Country</FormLabel>
-                    <FormControl>
-                        <Input placeholder={'Bangladesh'} {...field}/>
-                    </FormControl>
-                    <FormMessage/>
-                </FormItem>
-            )}/>
-            </div>
-            <FormField control={control} name={"DeliveryPrice"} render={({field}) => (
-                <FormItem className={'md:max-w-[25%]'}>
-                    <FormLabel>Delivery price (<FontAwesomeIcon icon={faBangladeshiTakaSign} />) </FormLabel>
-                    <FormControl>
-                        <Input placeholder={'1.50'} className={'bg-white'} {...field}/>
-                    </FormControl>
-                    <FormMessage/>
-                </FormItem>
-            )}/>
-            <FormField control={control} name={"Country"} render={({field}) => (
-                <FormItem className={'md:max-w-[25%]'}>
-                    <FormLabel>Estimated Delivery Time (minutes)</FormLabel>
-                    <FormControl>
-                        <Input placeholder={'25'} className={'bg-white'} {...field}/>
-                    </FormControl>
-                    <FormMessage/>
-                </FormItem>
-            )}/>
-            <DevTool control={control}/>
-        </div>
-    )
-}
-export default DetailsSection
+	const { control } = useFormContext();
+	return (
+		<div className={"space-y-2"}>
+			<div>
+				<h1 className={"text-2xl font-bold"}>Details</h1>
+			</div>
+			<FormDescription>Enter the details about your restaurant</FormDescription>
+			<FormField
+				control={control}
+				name={"RestaurantName"}
+				render={({ field }) => (
+					<FormItem>
+						<FormLabel>Name</FormLabel>
+						<FormControl>
+							<Input placeholder={"kabbo ghosh"} {...field} />
+						</FormControl>
+						<FormMessage />
+					</FormItem>
+				)}
+			/>
+			<div className={"flex gap-4"}>
+				<FormField
+					control={control}
+					name={"City"}
+					render={({ field }) => (
+						<FormItem className={"flex-1"}>
+							<FormLabel>City</FormLabel>
+							<FormControl>
+								<Input placeholder={"Jashore"} {...field} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={control}
+					name={"Country"}
+					render={({ field }) => (
+						<FormItem className={"flex-1"}>
+							<FormLabel>Country</FormLabel>
+							<FormControl>
+								<Input placeholder={"Bangladesh"} {...field} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+			</div>
+			<FormField
+				control={control}
+				name={"DeliveryPrice"}
+				render={({ field }) => (
+					<FormItem className={"md:max-w-[25%]"}>
+						<FormLabel>
+							Delivery price (<FontAwesomeIcon icon={faBangladeshiTakaSign} />){" "}
+						</FormLabel>
+						<FormControl>
+							<Input placeholder={"1.50"} className={"bg-white"} {...field} />
+						</FormControl>
+						<FormMessage />
+					</FormItem>
+				)}
+			/>
+			<FormField
+				control={control}
+				name={"DeliveryTime"}
+				render={({ field }) => (
+					<FormItem className={"md:max-w-[25%]"}>
+						<FormLabel>Estimated Delivery Time (minutes)</FormLabel>
+						<FormControl>
+							<Input placeholder={"25"} className={"bg-white"} {...field} />
+						</FormControl>
+						<FormMessage />
+					</FormItem>
+				)}
+			/>
+			<DevTool control={control} />
+		</div>
+	);
+};
+export default DetailsSection;
