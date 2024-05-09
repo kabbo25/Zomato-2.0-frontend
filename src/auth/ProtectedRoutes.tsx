34f605ec2cbfaaf811e8152c68ade8aac8fb40ea
kabbo25 <kabbo25@gmail.com>
2024-05-09 19:40:48 +0600

    fix: fix reloading and navigation problem

--- src/auth/ProtectedRoutes.tsx
+++ src/auth/ProtectedRoutes.tsx
@@ -1,14 +1,13 @@
 import {useAuth0} from "@auth0/auth0-react";
 import {Navigate, Outlet} from "react-router-dom";
+import LoadSpinner from "@/components/LoadSpinner.tsx";
 
 const ProtectedRoutes = () => {
-    const {isAuthenticated} = useAuth0();
-    return (
-        isAuthenticated ? (<Outlet/>) :
-            (
-                <Navigate to={'/'} replace/>
-            )
-
-    )
+    const {isAuthenticated,isLoading} = useAuth0();
+    if(isLoading)
+        return <LoadSpinner/>
+    if(isAuthenticated)
+        return <Outlet/>
+    return <Navigate to={'/'} replace/>
 }
 export default ProtectedRoutes
