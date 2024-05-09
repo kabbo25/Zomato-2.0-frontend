4e00693a5380bc06c7d83a2598ba11795d607343
kabbo25 <kabbo25@gmail.com>
2024-05-09 19:40:10 +0600

    feat: implement preview of image with proper aspect ratio

--- src/forms/manage-restaurant-form/ImageSection.tsx
+++ src/forms/manage-restaurant-form/ImageSection.tsx
@@ -1,9 +1,11 @@
 import {useFormContext} from "react-hook-form";
 import {FormControl, FormDescription, FormField, FormItem, FormMessage} from "@/components/ui/form.tsx";
 import {Input} from "@/components/ui/input.tsx";
+import {AspectRatio} from "@/components/ui/aspect-ratio.tsx";
 
 const ImageSection = () => {
-    const {control}=useFormContext();
+    const {control,watch}=useFormContext();
+    const existingImage= watch("ImageUrl");
     return (
         <div className={'space-y-2'}>
             <div>
@@ -12,7 +14,16 @@ const ImageSection = () => {
                     Add an image that will be displayed on your restaurant listing in the search results. Adding a new image will overwrite the existing one.
                 </FormDescription>
             </div>
-            <div className={'flex flex-col gap-8 w-[50%]'}>
+            <div className={'flex flex-col gap-8 md:w-[50%]'}>
+                {existingImage && (
+                    <AspectRatio ratio={16 / 9}>
+                        <img
+                            src={existingImage}
+                            className="rounded-md object-contain h-full w-full"
+                            alt={'restaurant'}
+                        />
+                    </AspectRatio>
+                )}
                 <FormField
                     control={control}
                     name="ImageFile"
