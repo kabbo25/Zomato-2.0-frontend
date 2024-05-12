47ee70a3f97645765d51fd189b05c75fa32efd3e
kabbo25 <kabbo25@gmail.com>
2024-05-12 19:15:11 +0600

    fix: useRef to fix realod issue

--- src/auth/ProtectedRoutes.tsx
+++ src/auth/ProtectedRoutes.tsx
@@ -1,13 +1,15 @@
-import {useAuth0} from "@auth0/auth0-react";
-import {Navigate, Outlet} from "react-router-dom";
-import LoadSpinner from "@/components/LoadSpinner.tsx";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useRef } from "react";
+import { Navigate, Outlet } from "react-router-dom";
 
 const ProtectedRoutes = () => {
-    const {isAuthenticated,isLoading} = useAuth0();
-    if(isLoading)
-        return <LoadSpinner/>
-    if(isAuthenticated)
-        return <Outlet/>
-    return <Navigate to={'/'} replace/>
-}
-export default ProtectedRoutes
+	const { isAuthenticated, isLoading } = useAuth0();
+	const isLogin = useRef(false);
+	if (isLoading) return null;
+	if (isAuthenticated || isLogin.current) {
+		isLogin.current = true;
+		return <Outlet />;
+	}
+	return <Navigate to={"/"} replace />;
+};
+export default ProtectedRoutes;
